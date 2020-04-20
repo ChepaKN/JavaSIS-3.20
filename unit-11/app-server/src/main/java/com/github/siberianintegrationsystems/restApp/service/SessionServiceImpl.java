3e15825f0fa58d6b86329330027f1d089427a6e4
ChepaKN <kchepa1992@gmail.com>
2020-04-20 22:10:47 +0300

    Добавил валидацию сессии. Осталось сохранить выбранные ответы в базу и написать тесты.

--- unit-11/app-server/src/main/java/com/github/siberianintegrationsystems/restApp/service/SessionServiceImpl.java
+++ unit-11/app-server/src/main/java/com/github/siberianintegrationsystems/restApp/service/SessionServiceImpl.java
@@ -3,15 +3,21 @@ package com.github.siberianintegrationsystems.restApp.service;
 import com.github.siberianintegrationsystems.restApp.controller.dto.session.AnswerSessionDTO;
 import com.github.siberianintegrationsystems.restApp.controller.dto.session.QuestionSessionDTO;
 import com.github.siberianintegrationsystems.restApp.controller.dto.session.SessionDTO;
+import com.github.siberianintegrationsystems.restApp.data.AnswerRepository;
+import com.github.siberianintegrationsystems.restApp.data.QuestionRepository;
 import com.github.siberianintegrationsystems.restApp.data.SelectedAnswerRepository;
 import com.github.siberianintegrationsystems.restApp.data.SessionEventRepository;
-import com.github.siberianintegrationsystems.restApp.entity.SelectedAnswer;
+import com.github.siberianintegrationsystems.restApp.entity.Answer;
+import com.github.siberianintegrationsystems.restApp.entity.Question;
 import com.github.siberianintegrationsystems.restApp.entity.SessionEvent;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,53 +26,84 @@ public class SessionServiceImpl implements SessionService {
 
     private SessionEventRepository sessionEventRepository;
     private SelectedAnswerRepository selectedAnswerRepository;
+    private QuestionRepository questionRepository;
+    private AnswerRepository answerRepository;
 
 
     public SessionServiceImpl(SessionEventRepository sessionEventRepository,
-                              SelectedAnswerRepository selectedAnswerRepository) {
+                              SelectedAnswerRepository selectedAnswerRepository,
+                              QuestionRepository questionRepository,
+                              AnswerRepository answerRepository) {
         this.sessionEventRepository = sessionEventRepository;
         this.selectedAnswerRepository = selectedAnswerRepository;
+        this.questionRepository = questionRepository;
+        this.answerRepository = answerRepository;
     }
 
+
+
+    private double validateOneQuestion(QuestionSessionDTO questionSessionDTO){
+        //Результат в диапазоне [0:1]
+        Question question = questionRepository
+                .findById(Long.parseLong(questionSessionDTO.id))
+                .orElseThrow(() ->
+                        new RuntimeException(String.format("Не найдем вопрос с id%s",
+                                questionSessionDTO.id)));
+
+        //всего ответов
+        double n = questionSessionDTO.answersList.size();
+        //всего верных для этого вопроса
+        double m = answerRepository.findByQuestion(question)
+                .stream()
+                .filter(Answer::getCorrect)
+                .count();
+
+        //верно выбранных
+        double k = 0;
+        //неверно выбранных
+        double w = 0;
+        List<AnswerSessionDTO> selectedAnswers = questionSessionDTO.answersList;
+
+//        for(int i = 0; i < n; i++){
+        for(AnswerSessionDTO selectedAns : selectedAnswers){
+//            AnswerSessionDTO selectedAns = selectedAnswers.get(i);
+            Answer savedAns = answerRepository.findById(Long.parseLong(selectedAns.id))
+                    .orElseThrow(() -> new RuntimeException(
+                            String.format("Не найден ответ с id: %s", selectedAns.id)));
+
+            if(selectedAns.isSelected){
+                if(savedAns.getCorrect()){
+                    k++;
+                }else{
+                    w++;
+                }
+            }
+            //todo: как быть с невыбранными верными ответами?
+        }
+        return Math.max(0, k/m - w/(n-m));
+    }
     @Override
     public String validateSession(SessionDTO sessionDTO){
-        String validateResult = "99.99";
+        Double result = sessionDTO.questionsList.stream()
+                .map(this::validateOneQuestion).reduce(0.0, Double::sum);
 
-        SessionEvent sessionEvent = new SessionEvent(sessionDTO.name,
-                Double.parseDouble(validateResult));
+        result = result / sessionDTO.questionsList.size() * 100.0;
 
         //сохраним сессию
-        sessionEventRepository.save(sessionEvent);
+        sessionEventRepository.save(new SessionEvent(sessionDTO.name, result));
 
         //сохраним выбранные ответы
-//        SelectedAnswer selectedAnswer = new SelectedAnswer();
-//        selectedAnswer.setSessionEvent(sessionEvent);
-//        selectedAnswer.setAnswer(sessionEvent.);
-
-//        List<AnswerSessionDTO> answerSessionDTOList = sessionDTO.questionsList
-//                .stream()
-//                .map(questionSessionDTO -> questionSessionDTO.answersList
-//                        .stream()
-//                        .filter(answerSessionDTO -> answerSessionDTO.isSelected))
-//                        .collect(Collectors.toList()));
-
         List<AnswerSessionDTO> answerSessionDTOS = new ArrayList<>();
 
-for(QuestionSessionDTO q : sessionDTO.questionsList) {
-    answerSessionDTOS.addAll(q
-            .answersList.stream()
-            .filter(answerSessionDTO -> answerSessionDTO.isSelected)
-            .collect(Collectors.toList()));
-}
-        return validateResult;
+        for(QuestionSessionDTO q : sessionDTO.questionsList) {
+            answerSessionDTOS.addAll(q
+                    .answersList.stream()
+                    .filter(answerSessionDTO -> answerSessionDTO.isSelected)
+                    .collect(Collectors.toList()));
+        }
+        //Locale.US для того чтобы при преобразовании в строку разделитель был точкой а не запятой, иначе клиент ругается
+        return String.format(Locale.US, "%.2f", result);
     }
-
-//    @Override
-//    public void saveSelectedAnswers(SessionDTO sessionDTO){
-//        SelectedAnswer selectedAnswer = new SelectedAnswer();
-//        selectedAnswer.setSessionEvent();
-//        sessionDTO.
-//    }
 
     @Override
     public SessionEvent getSession(String id) {
